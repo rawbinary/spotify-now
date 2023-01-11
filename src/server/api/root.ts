10ba848e2d9efe5f8faa27d6d36a7bb98e284b10
Robin Panta <hacktivistic@gmail.com>
2023-01-11 21:48:45 +0530

    create spotify trpc router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
+import { spotifyRouter } from "./routers/spotify";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "./routers/example";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  spotify: spotifyRouter,
 });
 
 // export type definition of API
