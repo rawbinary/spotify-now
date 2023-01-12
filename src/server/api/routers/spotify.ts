f7b8f6f7f74c12505a76dc4958ecee77d3b05ee8
Robin Panta <hacktivistic@gmail.com>
2023-01-12 07:39:39 +0530

    Create getCurrentSong tRPC router

--- src/server/api/routers/spotify.ts
+++ src/server/api/routers/spotify.ts
@@ -1,21 +1,45 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const spotifyRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
+  getCurrentSong: protectedProcedure.query(async ({ ctx }) => {
+    const accounts = await ctx.prisma.user
+      .findUnique({
+        where: { id: ctx.session.user.id },
+      })
+      .accounts();
+    const token = accounts?.pop()?.access_token;
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
+    if (!token) return "Token Expired re-login.";
 
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
+    const resp = await fetch(
+      "https://api.spotify.com/v1/me/player/currently-playing",
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      }
+    );
+
+    const trackInfo = (await resp.json()) as SpotifyCurrentlyPlayingTrack;
+    console.log(trackInfo);
+    return `${trackInfo.item.name} by ${
+      trackInfo.item.artists[0]
+        ? trackInfo.item.artists[0].name
+        : "Not artist available."
+    }`;
   }),
 });
+
+const SpotifyCurrentlyPlayingTrack = z.object({
+  is_playing: z.boolean(),
+  item: z.object({
+    artists: z.array(z.object({ name: z.string() })).length(1),
+    name: z.string(),
+  }),
+});
+
+type SpotifyCurrentlyPlayingTrack = z.infer<
+  typeof SpotifyCurrentlyPlayingTrack
+>;
