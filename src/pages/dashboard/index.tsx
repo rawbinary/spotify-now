ffaf85e34da0652e4923b675473434a9d2fa3be2
Robin Panta <hacktivistic@gmail.com>
2023-01-21 22:44:34 +0530

    protect dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,13 @@
 import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
+import AccessDenied from "../../components/AccessDenied";
 
 const Dashboard: NextPage = () => {
+    const { data: session } = useSession()
+
+    if (!session) {
+        return <AccessDenied/>
+    }
     return <>This is Dashboard</>
 }
 
