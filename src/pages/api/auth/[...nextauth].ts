e0b20d60abc7e0726b900798ddc2174a42935ac2
Robin Panta <hacktivistic@gmail.com>
2023-01-21 22:33:00 +0530

    temp: remove ref token refresh for spotify

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -12,59 +12,59 @@ import { prisma } from "../../../server/db";
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
-    async session({ session, user }) {
-      const [spotify] = await prisma.account.findMany({
-        where: { userId: user.id, provider: "spotify" },
-      });
+    session({ session, user }) {
+      // const [spotify] = await prisma.account.findMany({
+      //   where: { userId: user.id, provider: "spotify" },
+      // });
 
-      if (
-        spotify &&
-        spotify.expires_at &&
-        spotify.expires_at <= Math.ceil(Date.now() / 1000)
-      ) {
-        // token expired; trying to refresh it
-        try {
-          const resp = await fetch("https://accounts.spotify.com/api/token", {
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: new URLSearchParams({
-              client_id: env.SPOTIFY_CLIENT_ID,
-              client_secret: env.SPOTIFY_CLIENT_SECRET,
-              grant_type: "refresh_token",
-              refresh_token: spotify.refresh_token,
-            } as Record<string, string>),
-            method: "POST",
-          });
+      // if (
+      //   spotify &&
+      //   spotify.expires_at &&
+      //   spotify.expires_at <= Math.ceil(Date.now() / 1000)
+      // ) {
+      //   // token expired; trying to refresh it
+      //   try {
+      //     const resp = await fetch("https://accounts.spotify.com/api/token", {
+      //       headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      //       body: new URLSearchParams({
+      //         client_id: env.SPOTIFY_CLIENT_ID,
+      //         client_secret: env.SPOTIFY_CLIENT_SECRET,
+      //         grant_type: "refresh_token",
+      //         refresh_token: spotify.refresh_token,
+      //       } as Record<string, string>),
+      //       method: "POST",
+      //     });
 
-          const tokens = (await resp.json()) as {
-            access_token: string;
-            expires_in: number;
-            refresh_token: string;
-          };
+      //     const tokens = (await resp.json()) as {
+      //       access_token: string;
+      //       expires_in: number;
+      //       refresh_token: string;
+      //     };
 
-          if (!resp.ok) throw tokens;
+      //     if (!resp.ok) throw tokens;
 
-          await prisma.account.update({
-            where: {
-              provider_providerAccountId: {
-                provider: "spotify",
-                providerAccountId: spotify.providerAccountId,
-              },
-            },
-            data: {
-              access_token: tokens.access_token,
-              expires_at: Math.ceil(Date.now() / 1000) + tokens.expires_in,
-              refresh_token: tokens.refresh_token,
-            },
-          });
-        } catch (error) {
-          console.error("Error refreshing token", error);
-          session.error = "RefreshAccessTokenError";
-        }
-      }
+      //     await prisma.account.update({
+      //       where: {
+      //         provider_providerAccountId: {
+      //           provider: "spotify",
+      //           providerAccountId: spotify.providerAccountId,
+      //         },
+      //       },
+      //       data: {
+      //         access_token: tokens.access_token,
+      //         expires_at: Math.ceil(Date.now() / 1000) + tokens.expires_in,
+      //         refresh_token: tokens.refresh_token,
+      //       },
+      //     });
+      //   } catch (error) {
+      //     console.error("Error refreshing token", error);
+      //     session.error = "RefreshAccessTokenError";
+      //   }
+      // }
 
       if (session.user) {
         session.user.id = user.id;
-        session.user.accessToken = spotify?.access_token;
+        // session.user.accessToken = spotify?.access_token;
       }
 
       return session;
