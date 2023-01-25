6ad814f873f2bbc523ef7d4cf55ad7bacb83da67
Robin Panta <hacktivistic@gmail.com>
2023-01-25 19:04:38 +0530

    change doc layout

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,9 +6,7 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head />
         <body className="min-h-screen bg-gradient-to-b from-[#2e026d] to-[#15162c] font-sans text-white">
-          <main className="flex min-h-screen flex-col items-center justify-center">
-            <Main />
-          </main>
+          <Main />
           <NextScript />
         </body>
       </Html>
