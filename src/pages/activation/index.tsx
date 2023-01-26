6792ff267b4aaf5eec3bb0f143dd455730633b36
Robin Panta <hacktivistic@gmail.com>
2023-01-26 21:01:00 +0530

    import prisma

--- src/pages/activation/index.tsx
+++ src/pages/activation/index.tsx
@@ -7,6 +7,7 @@ import type {
 import { getServerAuthSession } from "../../server/auth";
 import type { Session } from "next-auth";
 import { useSession } from "next-auth/react";
+import { prisma } from "../../server/db";
 
 type Props = {
   session: Session;
@@ -60,7 +61,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
     };
   }
 
-  const user = await prisma?.user.findUnique({
+  const user = await prisma.user.findFirst({
     where: { id: session.user?.id },
   });
 
