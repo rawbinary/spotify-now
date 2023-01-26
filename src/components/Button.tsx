53309fe336d6611e562989588f41c9ad3bb53cd4
Robin Panta <hacktivistic@gmail.com>
2023-01-27 00:17:01 +0530

    add click event handler

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,19 @@
-import type { PropsWithChildren } from "react";
+import type { MouseEventHandler, PropsWithChildren } from "react";
 
 type ButtonProps = PropsWithChildren & {
-  className: string;
+  className?: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
-export default function Button({ className, children }: ButtonProps) {
+export default function Button({
+  className = "",
+  onClick,
+  children,
+}: ButtonProps) {
   return (
     <button
       className={`${className} rounded-md bg-zinc-800 px-2 py-2 outline-none duration-200 ease-in-out hover:text-gray-300 hover:transition focus:ring-1 focus:ring-gray-500 focus:ring-offset-1 focus:ring-offset-stone-800`}
+      onClick={onClick}
     >
       {children}
     </button>
