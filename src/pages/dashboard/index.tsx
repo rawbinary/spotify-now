19102ab65d8d8aa5cfdec3ef267e220583fdb4bf
Robin Panta <hacktivistic@gmail.com>
2023-01-27 00:18:07 +0530

    show playing content

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,18 +6,39 @@ import type {
 
 import DashboardLayout from "../../layout/dashboard";
 import { getServerAuthSession } from "../../server/auth";
+import { api } from "../../utils/api";
+import { signIn, useSession } from "next-auth/react";
+import Button from "../../components/Button";
 
 const Dashboard: NextPage = () => {
+  const { data: spotifyUser } = api.spotify.getCurrentUser.useQuery();
+  const { data: session } = useSession();
+  const { data: currSong } = api.spotify.getCurrentSong.useQuery();
   return (
     <DashboardLayout>
       <main className="flex flex-col items-center justify-center pt-10 transition-all duration-100">
         <div className="flex flex-col items-center justify-center gap-6 rounded-lg border border-zinc-800 p-4 hover:shadow-lg">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[2rem]">
-            Now Playing
-          </h1>
-          <h3 className="text-2xl text-white">
-            Sign In to show your currently playing song on spotify
-          </h3>
+          {spotifyUser && (
+            <>
+              <h3 className="text-2xl text-violet-400">
+                Hi, {session?.user?.name}!
+              </h3>
+              <h1 className="text-5xl font-extrabold tracking-tight text-green-400 sm:text-[2rem]">
+                Now Playing
+              </h1>
+              <h3 className="text-2xl text-white">{currSong}</h3>
+            </>
+          )}
+          {!spotifyUser && (
+            <>
+              <h3 className="text-xl text-green-200">
+                Not authorized to access Spotify.
+              </h3>
+              <Button onClick={() => void signIn("spotify")}>
+                Authorize Spotify Access
+              </Button>
+            </>
+          )}
         </div>
       </main>
     </DashboardLayout>
