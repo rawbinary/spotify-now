323dec6542018bef737c6c88d79c36f135a0250c
Robin Panta <hacktivistic@gmail.com>
2023-01-27 06:39:18 +0530

    get spotify auth in server side props add browser source link copy button

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,35 +1,61 @@
 import type {
   GetServerSideProps,
   GetServerSidePropsContext,
+  InferGetServerSidePropsType,
   NextPage,
 } from "next";
 
 import DashboardLayout from "../../layout/dashboard";
 import { getServerAuthSession } from "../../server/auth";
 import { api } from "../../utils/api";
-import { signIn, useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import Button from "../../components/Button";
 
-const Dashboard: NextPage = () => {
-  const { data: spotifyUser } = api.spotify.getCurrentUser.useQuery();
-  const { data: session } = useSession();
-  const { data: currSong } = api.spotify.getCurrentSong.useQuery();
+const Dashboard: NextPage = (
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) => {
+  const {
+    data: currSong,
+    isError,
+    error,
+  } = api.spotify.getCurrentSong.useQuery();
+
+  const {
+    data,
+    error: linkError,
+    refetch,
+  } = api.spotify.getStreamLink.useQuery(undefined, { enabled: false });
+
+  const copyToClipboard = async () => {
+    await refetch();
+    if (linkError) return alert(linkError);
+    if (data) {
+      await navigator.clipboard.writeText(data);
+      alert("Link copied!");
+    }
+  };
+
   return (
     <DashboardLayout>
       <main className="flex flex-col items-center justify-center pt-10 transition-all duration-100">
         <div className="flex flex-col items-center justify-center gap-6 rounded-lg border border-zinc-800 p-4 hover:shadow-lg">
-          {spotifyUser && (
+          {props.spotify && (
             <>
-              <h3 className="text-2xl text-violet-400">
-                Hi, {session?.user?.name}!
-              </h3>
+              <h3 className="text-2xl text-violet-400">Hi, {props.user}!</h3>
               <h1 className="text-5xl font-extrabold tracking-tight text-green-400 sm:text-[2rem]">
                 Now Playing
               </h1>
               <h3 className="text-2xl text-white">{currSong}</h3>
+              <Button onClick={() => void copyToClipboard()}>
+                Get Browser Source URL
+              </Button>
+              <span className="text-sm">
+                Careful, anyone with this link can get your spotify now playing
+                status.
+              </span>
             </>
           )}
-          {!spotifyUser && (
+          {!props.spotify && !isError && (
             <>
               <h3 className="text-xl text-green-200">
                 Not authorized to access Spotify.
@@ -39,11 +65,22 @@ const Dashboard: NextPage = () => {
               </Button>
             </>
           )}
+          {isError && (
+            <>
+              <p>
+                Error Occurred, Please raise a issue at
+                https://github.com/rawbinary/spotify-now:{" "}
+              </p>
+              <pre>{error.message}</pre>
+            </>
+          )}
         </div>
       </main>
     </DashboardLayout>
   );
 };
+
+import { prisma } from "../../server/db";
 
 export const getServerSideProps: GetServerSideProps = async (
   ctx: GetServerSidePropsContext
@@ -68,8 +105,12 @@ export const getServerSideProps: GetServerSideProps = async (
     };
   }
 
+  const accs = await prisma.account.count({
+    where: { userId: session.user.id, provider: "spotify" },
+  });
+
   return {
-    props: { session },
+    props: { user: session.user.name, spotify: accs > 0 },
   };
 };
 
